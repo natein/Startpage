54a8797afb89337ac3ef4dc4ede9b9f62085a064
Natein <natein@gmail.com>
2020-12-27 03:33:25 +0200

    feat: data & utils

---
+++ src/js/data/constants.js
@@ -0,0 +1,15 @@
+export const APP_NAME = 'Start Page';
+
+export const DEVELOPERS = [
+  {
+    user: 'natein',
+    link: 'https://github.com/natein',
+  },
+  {
+    user: 'vadim-bykov',
+    link: 'https://github.com/vadim-bykov',
+  },
+];
+
+export const COURSE_LINK = 'https://rs.school/js/';
+export const COURSE_NAME = 'JavaScript/Front-end 2020Q3';
