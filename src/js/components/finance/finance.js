6bbf1d8d660e40bf0baf2276d4def0e8370715ee
Vadim <bvntaev@gmail.com>
2020-12-28 00:04:22 +0300

    feat: get currency data

--- src/js/components/finance/finance.js
+++ src/js/components/finance/finance.js
@@ -1,4 +1,5 @@
-import './finance.scss';
+// import './finance.scss';
+import "./finance.css";
 
 class Finance {
   constructor(parentNode) {
@@ -8,9 +9,38 @@ class Finance {
   }
 
   render() {
-    const caption = document.createElement('h3');
-    caption.textContent = 'Finance';
+    const caption = document.createElement("h3");
+    caption.textContent = "Finance";
     this.parentNode.appendChild(caption);
+
+    async function getFinanceData() {
+      const response = await fetch(
+        "https://www.nbrb.by/api/exrates/rates?periodicity=0"
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    // getFinanceData();
+
+    const currentDate = new Date();
+    const dataDate = currentDate.toString().split(' ');
+    const day = dataDate[0];
+    const month = dataDate[1];
+    const date = dataDate[2];
+    const year = dataDate[3];
+    // console.log(day, month, date, year)
+    // console.log(currentDate.toString().split(' '))
+
+    async function getCurrencyData(Cur_ID = 292) {
+      const response = await fetch(
+        `https://www.nbrb.by/API/ExRates/Rates/Dynamics/${Cur_ID}?startDate=Tue%2C+31+Dec+2019+21%3A00%3A00+GMT&endDate=${day}%2C+${date}+${month}+${year}+21%3A00%3A00+GMT`
+      );
+      const data = await response.json();
+      console.log(data);
+    };
+    // getCurrencyData('145');
+
+
   }
 }
 
