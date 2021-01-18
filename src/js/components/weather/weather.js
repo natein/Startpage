e60a9548fa1617f3b5f8117eac4cf5df49dc060d
Vadim <bvntaev@gmail.com>
2021-01-18 19:46:57 +0300

    feat: update weather data

--- src/js/components/weather/weather.js
+++ src/js/components/weather/weather.js
@@ -2,88 +2,97 @@
 import './weather.css';
 import './owfont-regular.css';
 
-// const weatherIcon = document.querySelector('.weather-icon');
-// const temperature = document.querySelector('.temperature');
-// const humidity = document.querySelector('.humidity');
-// const wind = document.querySelector('.wind');
-// const weatherDescription = document.querySelector('.weather-description');
-// const city = document.querySelector('.city');
-
-async function getWeather(e) {
-  const weatherIcon = document.querySelector('.weather-icon');
-  const temperature = document.querySelector('.temperature');
-  const humidity = document.querySelector('.humidity');
-  const wind = document.querySelector('.wind');
-  const weatherDescription = document.querySelector('.weather-description');
-  const city = document.querySelector('.city');
-  try {
-    if (localStorage.getItem('city') === null) {
-      city.textContent = 'Минск';
-    } else {
-      city.textContent = localStorage.getItem('city');
-    }
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=543c9bb3c115ee39476506208d454b41&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-
-    weatherIcon.className = 'weather-icon owf';
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}°C`;
-    humidity.textContent = `Влажность воздуха ${data.main.humidity} %`;
-    wind.textContent = `Скорость ветра ${data.wind.speed} м/с`;
-    weatherDescription.textContent = data.weather[0].description;
-
-    return data;
-  } catch (error) {
-    city.textContent = 'Введите правильно город';
-  }
-}
-// getWeather()
-// .then(res => console.log(res))
-// .catch(err => console.log(err));
-
-function setCity(e) {
-  const city = document.querySelector('.city');
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      city.blur();
-      if (e.target.innerText != '' && e.target.innerText != 'Минск') {
-        localStorage.setItem('city', e.target.innerText);
-        city.blur();
-        getWeather();
-      }
-    }
-  } else {
-    if (e.target.innerText == '') {
-      city.textContent = localStorage.getItem('city') || 'Минск';
-    } else {
-      localStorage.setItem('city', e.target.innerText);
-      getWeather();
-    }
-  }
-}
-
-// // document.addEventListener('DOMContentLoaded', getWeather);
-// city.addEventListener('keypress', setCity);
-// city.addEventListener('blur', setCity);
-// city.addEventListener('click', () => city.textContent = '');
-
 class Weather {
   constructor(parentNode) {
     this.parentNode = parentNode;
     this.render();
   }
 
+  getDomElements() {
+    const weatherIcon = this.parentNode.querySelector('.weather-icon');
+    const temperature = this.parentNode.querySelector('.temperature');
+    const humidity = this.parentNode.querySelector('.humidity');
+    const wind = this.parentNode.querySelector('.wind');
+    const weatherDescription = this.parentNode.querySelector(
+      '.weather-description'
+    );
+    const city = this.parentNode.querySelector('.city');
+
+    return {
+      weatherIcon,
+      temperature,
+      humidity,
+      wind,
+      weatherDescription,
+      city,
+    };
+  }
+
   addListeners() {
-    const city = document.querySelector('.city');
-    // document.addEventListener('DOMContentLoaded', getWeather);
-    console.log(city);
-    city.addEventListener('keypress', setCity);
-    city.addEventListener('blur', setCity);
+    const { city } = this.getDomElements();
+    city.addEventListener('keypress', this.setCity.bind(this));
+    city.addEventListener('blur', this.setCity.bind(this));
     city.addEventListener('click', () => (city.textContent = ''));
-  };
+  }
+
+  async getWeatherData() {
+    const { city } = this.getDomElements();
+
+    if (localStorage.getItem('city') === null) {
+      city.textContent = 'Minsk';
+    } else {
+      city.textContent = localStorage.getItem('city');
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=543c9bb3c115ee39476506208d454b41&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    if (!res.ok) city.textContent = 'Enter correct city';
+
+    return data;
+  }
+
+  async fillWeatherBlock() {
+    const data = await this.getWeatherData();
+    const {
+      weatherIcon,
+      temperature,
+      humidity,
+      wind,
+      weatherDescription,
+    } = this.getDomElements();
+
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;
+    humidity.textContent = `Humidity ${data.main.humidity} %`;
+    wind.textContent = `Wind speed ${data.wind.speed} m/s`;
+    weatherDescription.textContent = data.weather[0].description;
+  }
+
+  setCity(e) {
+    const { city } = this.getDomElements();
+    const pressedEnter = e.which === 13 || e.keyCode === 13;
+    const cityEntered =
+      e.target.innerText !== '' && e.target.innerText !== 'Минск';
+    this.fillWeatherBlock.bind(this);
+    if (e.type === 'keypress') {
+      if (pressedEnter) {
+        city.blur();
+        if (cityEntered) {
+          localStorage.setItem('city', e.target.innerText);
+          city.blur();
+          this.fillWeatherBlock();
+        }
+      }
+    } else {
+      if (e.target.innerText === '') {
+        city.textContent = localStorage.getItem('city') || 'Минск';
+      } else {
+        localStorage.setItem('city', e.target.innerText);
+        this.fillWeatherBlock();
+      }
+    }
+  }
 
   render() {
     this.parentNode.innerHTML = `
@@ -97,7 +106,7 @@ class Weather {
       <div class="weather-description"></div>
     </div>
     `;
-    getWeather();
+    this.fillWeatherBlock();
     this.addListeners();
   }
 }
